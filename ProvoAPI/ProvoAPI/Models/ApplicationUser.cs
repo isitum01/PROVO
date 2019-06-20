c84fc791f5048c251ba3dcb215a92f3d23037d85
isitum01 <isitum01@gmail.com>
2019-06-20 14:01:39 +0200

    Initial commit

---
+++ ProvoAPI/ProvoAPI/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvoAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string aIme { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string aPrezime { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string aUsername { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string aPass { get; set; }
+    }
+}
---
+++ ProvoAPI/ProvoAPI/Models/ApplicationUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvoAPI.Models
+{
+    public class ApplicationUserModel
+    {
+        public string aIme { get; set; }
+        public string aPrezime { get; set; }
+        public string aUsername { get; set; }
+        public string aPass { get; set; }
+    }
+}
