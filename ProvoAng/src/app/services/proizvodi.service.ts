02bfb8027e82151ad2c3af9b166499d259ef1851
isitum01 <isitum01@gmail.com>
2019-06-27 19:33:47 +0200

    Create proizvodi.service.ts

---
+++ ProvoAng/src/app/services/proizvodi.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Proizvod } from '../models/proizvod.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProizvodiService {
+
+  formProizvod: Proizvod;
+  listPro: Proizvod[];
+  readonly rootURL = "http://localhost:50963/api";
+
+  constructor(private http: HttpClient) { }
+
+  postProizvod(formProizvod: Proizvod) {
+    return this.http.post(this.rootURL + '/Proizvodi', formProizvod);
+  }
+  getProizvod() {
+    this.http.get(this.rootURL + '/Proizvodi').subscribe(res => this.listPro = res as Proizvod[]);
+  }
+  putProizvod(formProizvod: Proizvod) {
+    return this.http.put(this.rootURL + '/Proizvodi/' + formProizvod.pID, formProizvod);
+  }
+  deleteProizvod(id: number) { 
+    return this.http.delete(this.rootURL + '/Proizvodi/' + id);
+  }
+}
