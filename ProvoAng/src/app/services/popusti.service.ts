06de02bb08aea6bcaa8626cd59d004f1dbe875c1
isitum01 <isitum01@gmail.com>
2019-06-27 19:33:41 +0200

    Create popusti.service.ts

---
+++ ProvoAng/src/app/services/popusti.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Popust } from '../models/popust.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PopustiService {
+
+  formPopust: Popust;
+  listPop: Popust[];
+  readonly rootURL = "http://localhost:50963/api";
+
+  constructor(private http: HttpClient) { }
+
+  postPopust(formPopust: Popust) {
+    return this.http.post(this.rootURL + '/Popusti', formPopust);
+  }
+  getPopust() {
+    this.http.get(this.rootURL + '/Popusti').subscribe(res => this.listPop = res as Popust[]);
+  }
+  putPopust(formPopust: Popust) {
+    return this.http.put(this.rootURL + '/Popusti/' + formPopust.popID, formPopust);
+  }
+  deletePopust(id: number) { 
+    return this.http.delete(this.rootURL + '/Popusti/' + id);
+  }
+}
