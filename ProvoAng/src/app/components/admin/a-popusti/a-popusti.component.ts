2135ccf29b1a56cd4303d0ec146e55d6ccc7c66d
isitum01 <isitum01@gmail.com>
2019-06-27 19:32:59 +0200

    Update a-popusti.component.ts

--- ProvoAng/src/app/components/admin/a-popusti/a-popusti.component.ts
+++ ProvoAng/src/app/components/admin/a-popusti/a-popusti.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PopustiService } from 'src/app/services/popusti.service';
+import { NgForm } from '@angular/forms';
+import { Popust } from 'src/app/models/popust.model';
 
 @Component({
   selector: 'app-a-popusti',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class APopustiComponent implements OnInit {
 
-  constructor() { }
+  constructor(private servicePop: PopustiService) { }
 
   ngOnInit() {
+    this.servicePop.getPopust();
+    this.resetForm();
+  }
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+    this.servicePop.formPopust = {
+      popID: null,
+      popKod: '',
+      popIznos: null
+    }
   }
 
+  onSubmit(form: NgForm) {
+    if (form.value.pID == null){
+      this.insertPopust(form);
+    }
+    else
+      this.updatePopust(form);
+  }
+
+  insertPopust(form: NgForm) {
+    this.servicePop.postPopust(form.value).subscribe(res => {
+      this.servicePop.getPopust();
+      this.resetForm(form);
+    });
+  }
+  editPopust(pro: Popust) {
+    this.servicePop.formPopust = Object.assign({},pro);
+  }
+  updatePopust(form: NgForm) {
+    this.servicePop.putPopust(form.value).subscribe(res => {
+      this.resetForm(form);
+      this.servicePop.getPopust();
+    });
+    console.log("Updated");
+  }
+  removePopust(id: number){
+    this.servicePop.deletePopust(id).subscribe(res =>{
+    this.servicePop.getPopust();
+    });
+  }
 }
