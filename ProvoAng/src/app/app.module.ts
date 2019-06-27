aa0ea7738e7d785e18e023776ada0a7cc42b663a
isitum01 <isitum01@gmail.com>
2019-06-27 22:54:38 +0200

    Update app.module.ts

--- ProvoAng/src/app/app.module.ts
+++ ProvoAng/src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { KorisniciService } from './services/korisnici.service';
+import { KosaricaService } from './services/kosarica.service';
+import { NarudzbeService } from './services/narudzbe.service';
+import { PopustiService } from './services/popusti.service';
+import { ProizvodiService } from './services/proizvodi.service';
+import { ANarudzbeComponent } from './components/admin/a-narudzbe/a-narudzbe.component';
+import { AProizvodiComponent } from './components/admin/a-proizvodi/a-proizvodi.component';
+import { APopustiComponent } from './components/admin/a-popusti/a-popusti.component';
+import { AKorisniciComponent } from './components/admin/a-korisnici/a-korisnici.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    NavbarComponent,
+    ANarudzbeComponent,
+    AProizvodiComponent,
+    APopustiComponent,
+    AKorisniciComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    KorisniciService,
+    KosaricaService,
+    NarudzbeService,
+    PopustiService,
+    ProizvodiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
