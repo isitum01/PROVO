82afbbd2a36732fa0eac73a2eccc8409ba29dca5
isitum01 <isitum01@gmail.com>
2019-06-27 15:56:06 +0200

    Create a-popusti.component.ts

---
+++ ProvoAng/src/app/components/admin/a-popusti/a-popusti.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a-popusti',
+  templateUrl: './a-popusti.component.html',
+  styleUrls: ['./a-popusti.component.css']
+})
+export class APopustiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
