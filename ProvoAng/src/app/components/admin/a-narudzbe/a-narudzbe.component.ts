384d9835b16fbc0bea77320be739a4f96ff1caec
isitum01 <isitum01@gmail.com>
2019-06-27 22:54:56 +0200

    Create a-narudzbe.component.ts

---
+++ ProvoAng/src/app/components/admin/a-narudzbe/a-narudzbe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a-narudzbe',
+  templateUrl: './a-narudzbe.component.html',
+  styleUrls: ['./a-narudzbe.component.css']
+})
+export class ANarudzbeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
