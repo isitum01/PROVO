25ce099f1920ddfa1f00b80b24bd951be53cc791
isitum01 <isitum01@gmail.com>
2019-06-27 22:55:05 +0200

    Update korisnici.service.ts

--- ProvoAng/src/app/services/korisnici.service.ts
+++ ProvoAng/src/app/services/korisnici.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Korisnik } from '../models/korisnik.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KorisniciService {
+ 
   formKorisnik: Korisnik;
+  readonly rootUrl = "http://localhost:50963/api";
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postKorisnik(formKorisnik: Korisnik){
+    return this.http.post(this.rootUrl + '/Korisnici', formKorisnik);
+
+  }
 }
