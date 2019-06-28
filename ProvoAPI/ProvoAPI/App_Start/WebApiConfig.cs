182bb6400d2296697fd37f084290974f99c5204f
isitum01 <isitum01@gmail.com>
2019-06-28 18:47:13 +0200

    Update WebApiConfig.cs

--- ProvoAPI/ProvoAPI/App_Start/WebApiConfig.cs
+++ ProvoAPI/ProvoAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ProvoAPI
 {
@@ -9,6 +10,9 @@ namespace ProvoAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            //EnableCOR
+            config.EnableCors(new EnableCorsAttribute("http://localhost:4200",headers:"*",methods:"*"));
+
             // Web API configuration and services
 
             // Web API routes
